6e7152a59cc3ac3eca776a51abaeb287d59205d7
baton96 <podolce@op.pl>
2021-11-28 16:11:36 +0100

    Prettify response

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from RESTface import handler
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 root = {}
 
 
@@ -10,6 +11,10 @@ root = {}
 def index(path=None):
     if not path:
         result = root
+    elif path == 'favicon.ico':
+        return '', 404
     else:
         result = handler({'url': path}, request.method, root)
+        if request.method == 'DELETE':
+            return '', 204 if result else 404
     return jsonify(result)
