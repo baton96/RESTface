d1a7e33afaf7402b3d44eed7efff973542a069c5
baton96 <podolce@op.pl>
2021-12-22 01:57:36 +0100

    Updated app

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from flask import Flask, request, jsonify
 
-from RESTface import handler, root
+from RESTface import RESTface
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+face = RESTface()
 
 
 @app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 @app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def index(path=None):
     if not path:
-        result = root
+        result = face.all()
     elif path == 'favicon.ico':
         return '', 404
     else:
         body = request.get_json(force=True, silent=True) or {}
-        result = handler({'url': path, 'body': body}, request.method)
+        result = face.handler({'url': path, 'body': body}, request.method)
     return jsonify(result)
