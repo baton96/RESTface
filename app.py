0c18b691bcf060cd38aa3ba25edafac2d13d0c74
baton96 <podolce@op.pl>
2022-11-09 23:00:23 +0100

    Receiving files - innit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 
 from RESTface import RESTface
-from utils import reformat
+from utils import reformat, receive_file
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
@@ -12,6 +12,8 @@ face = RESTface()
 @app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 @app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def index(path=None):
+    if request.files:
+        receive_file()
     if path is None:
         result = face.all()
     elif path == 'favicon.ico':
@@ -22,3 +24,19 @@ def index(path=None):
     if 'format' in request.args:
         return reformat(result)
     return jsonify(result)
+
+
+# @app.route('/upload', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def upload():
+    if request.method == 'GET':
+        return '''
+            <form method=post enctype=multipart/form-data>
+                <input type=file name=file123 multiple>
+                <input type=submit value=Upload>
+            </form>'''
+    else:
+        if request.files:
+            file = next(iter(request.files.values()))
+            print(file.stream.read().decode("utf-8"))
+            print(file.filename)
+        return 'OK'
