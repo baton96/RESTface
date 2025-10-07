e1ff0579c7e293ed58d5689d8d830911d94f8aef
baton96 <podolce0@gmail.com>
2025-10-07 22:58:50 +0200

    Temporally disabled less popular storage options

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,16 @@ import pytest
 import RESTface
 
 
-@pytest.fixture(autouse=True, params=['memory', 'file', 'db', 'mongo', 'redis'])
+@pytest.fixture(
+    autouse=True,
+    params=[
+        'memory',
+        'file',
+        'db',
+        # 'mongo',
+        # 'redis'
+    ]
+)
 def face(request):
     _face = RESTface.RESTface(request.param)
     yield _face
